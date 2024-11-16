67071d9e8929f5e23c0fc2a9d30be511c8d9307c
sannitiainen <sanni.e.tiainen@helsinki.fi>
2024-11-16 23:40:38 +0200

    developed the function "register_route"

--- sovellus/routes.py
+++ sovellus/routes.py
@@ -1,10 +1,19 @@
 from app import app
-from flask import redirect, render_template, request, session
+from flask import redirect, render_template, request, session, flash
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/login")
-def login_route():
+@app.route("/login", methods=["GET", "POST"])
+def login():
     return render_template("login.html")
+
+@app.route("/register")
+def register_route():
+    return render_template("register.html")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
