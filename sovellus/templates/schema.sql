2b394aecfd4100e62c61295a45ee0db2badcc711
sannitiainen <sanni.e.tiainen@helsinki.fi>
2024-11-16 17:18:54 +0200

    created file schema.sql and created the tables for users, restaurants and reviews of the restaurants

---
+++ sovellus/templates/schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE, 
+    password TEXT, 
+    admin INTEGER
+    )
+;
+
+CREATE TABLE restaurants (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    openinghours TEXT,
+    address TEXT,
+    info TEXT);
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    restaurant_id INTEGER REFERENCES restaurants,
+    rating INTEGER,
+    comment TEXT);
