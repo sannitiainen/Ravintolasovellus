991e0d70847a144e675c7869e9c28486124d2eae
sannitiainen <sanni.e.tiainen@helsinki.fi>
2024-11-16 19:29:48 +0200

    Tried to create the login page

--- sovellus/users.py
+++ sovellus/users.py
@@ -1,31 +1,25 @@
+from app import app
 from flask import Flask
 from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
+from db import db
+from sqlalchemy.sql import text
+from os import getenv
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///user"
-db = SQLAlchemy(app)
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/login",methods=["POST"])
 def login():
     username = request.form["username"]
     password = request.form["password"]
 
-    # TODO: check username and password
+    #TODO: check username and password
 
-    usernames = db.session.execute("SELECT username FROM users")
-    users_list = usernames.fetchall()
+    usernames = db.session.execute(text("SELECT username FROM users"))
+    users_list = [row[0] for row in usernames.fetchall()]
     if username not in users_list:
-        return False   
+        return "Access denied: Invalid username", 401
 
     session["username"] = username
     return redirect("/")
 
-@app.route("/logout")
 def logout():
     del session["username"]
     return redirect("/")
