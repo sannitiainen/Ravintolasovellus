b4d797ab6f3980fb09454f91bdcd316517c8d84e
sannitiainen <sanni.e.tiainen@helsinki.fi>
2024-11-16 20:16:25 +0200

    added the login route to routes.py

--- sovellus/routes.py
+++ sovellus/routes.py
@@ -1,8 +1,10 @@
 from app import app
-import visits
-from flask import render_template
+from flask import redirect, render_template, request, session
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/login")
+def login_route():
+    return render_template("login.html")
