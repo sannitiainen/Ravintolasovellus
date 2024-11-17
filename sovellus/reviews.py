c63da3d0e84fabe2ae8cb3701a3a959b85946488
sannitiainen <sanni.e.tiainen@helsinki.fi>
2024-11-17 19:31:19 +0200

    modfied the functions

--- sovellus/reviews.py
+++ sovellus/reviews.py
@@ -1,9 +1,20 @@
 
-from flask import request, render_template
+from flask import request, render_template, result
 from db import db
+import users
 
 def add_review():
-    pass
+    restaurant_name = request.form["name"]
+    sql = "SELECT id FROM restaurants WHERE name:= restaurant_name"
+    result= db.session.execute(sql, {"username": restaurant_name})
+    r_id= result.fetchone()
+
+    user_id = users.user_id()
+    if user_id == 0:
+        return False
+
+    sql = "InSERT INTO reviews (user_id, restaurant_id, rating, comment) VALUES (:user_id, :restaurant_id, :rating, :comment)"
+    db.session.execute(sql, {""})
 
 def delete_review():
     #only if admin
